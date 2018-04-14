0c4af8c767f140d6fddb4e31ad66b553be8102c3
Florian Märkl <info@florianmaerkl.de>
2018-04-14 22:58:14 +0200

    Fix RPATH for bundled Python in AppImage

--- scripts/prepare_python_linux.sh
+++ scripts/prepare_python_linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir python && cd python
+mkdir -p python && cd python || exit 1
 
 wget "https://www.python.org/ftp/python/3.6.4/Python-3.6.4.tar.xz" || exit 1
 tar -xf Python-3.6.4.tar.xz || exit 1
@@ -13,6 +13,20 @@ echo "Building Python to install to prefix $CUSTOM_PYTHON_PREFIX"
 ./configure --enable-shared --prefix=$CUSTOM_PYTHON_PREFIX || exit 1
 make -j4 || exit 1
 make install > /dev/null || exit 1
+
+cd ..
+
+echo "Patching libs in $CUSTOM_PYTHON_PREFIX/lib/python3.6/lib-dynload to have the correct rpath"
+wget https://nixos.org/releases/patchelf/patchelf-0.9/patchelf-0.9.tar.bz2 || exit 1
+tar -xf patchelf-0.9.tar.bz2 || exit 1
+cd patchelf-0.9 || exit 1
+./configure || exit 1
+make || exit 1
+cd ..
+
+for lib in "$CUSTOM_PYTHON_PREFIX/lib/python3.6/lib-dynload"/*.so; do
+    patchelf/src/patchelf --set-rpath '$ORIGIN/../..' "$lib"
+done
 
 PYTHONHOME=$CUSTOM_PYTHON_PREFIX \
        LD_LIBRARY_PATH=$CUSTOM_PYTHON_PREFIX/lib \
